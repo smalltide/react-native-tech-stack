16b7a30d58ce33d1bb3d25a33287442b8a0a8840
ice <smalltides@gmail.com>
2016-11-16 17:40:35 +0800

    add ListView and ListItem to show libraries data

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import LibraryList from './components/LibraryList';
 
 const App = () => (
   <Provider store={createStore(reducers)}>
-    <View>
+    <View style={{ flex: 1 }}>
       <Header headerText="Tech Stack" />
       <LibraryList />
     </View>
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+    console.log(this.dataSource);
+  }
+
+  renderRow(item) {
+    return <ListItem item={item} />;
+  }
+
   render() {
-    return <View />;
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
